462a87557b0da092228d8ca257c4b7d37dd066f5
Marcus Schumann <Tanax.ms@gmail.com>
2012-09-01 14:09:19 +0200

    update

--- views/custom/blog/sectionview.js
+++ views/custom/blog/sectionview.js
@@ -12,7 +12,7 @@ var BlogSectionView = SectView.extend({
 		var page = this.model.get( 'activePage' );
 		var link = '#' + page;
 
-		$('.contact-back a, .message-back a').attr('href', link);
+		$('.about-back a, .message-back a').attr('href', link);
 		// Change links if we have more pages in current section
 		// Disable links: app.disableLinks();
 
